33bed6754d1adf1f850b5e161923b004e5f464e6
Zach Olkin <zach.olkin@gmail.com>
2025-05-01 09:19:47 -0700

    save value data to a file and read it out and plot it

--- gpc/value_learning/example_baseline.py
+++ gpc/value_learning/example_baseline.py
@@ -4,12 +4,16 @@ from hydrax.algs import Evosax, PredictiveSampling, MPPI, CEM
 
 from gpc.envs import PendulumEnv
 from gpc.training import train
-from gpc.value_learning.value_baseline import compute_baseline
+from gpc.value_learning.value_baseline import compute_baseline, parse_value_data
 
 if __name__ == "__main__":
     horizon = 1.0
     knots = int(horizon * 10)
-    iters = 250 #1
+    iters = 1 #5 #1
+
+    num_compute = 1000
+
+    filename = "data/value_data_2.pkl"
 
     env = PendulumEnv(episode_length=200)
     ctrl = Evosax(env.task,
@@ -20,34 +24,35 @@ if __name__ == "__main__":
                   num_knots=knots,
                   iterations=iters,
                   spline_type="zero")
-    compute_baseline(ctrl, env)
+    compute_baseline(filename, ctrl, env, num_compute)
+    parse_value_data(filename)
 
-    ctrl_ps = PredictiveSampling(env.task,
-                  num_samples=256,
-                  plan_horizon=horizon,
-                  noise_level=0.1,
-                  num_knots=knots,
-                  iterations=iters,
-                  spline_type="zero")
-    compute_baseline(ctrl_ps, env)
-
-    ctrl_mppi = MPPI(env.task,
-                  num_samples=256,
-                  plan_horizon=horizon,
-                  noise_level=0.1,
-                  temperature=0.1,
-                  num_knots=knots,
-                  iterations=iters,
-                  spline_type="zero")
-    compute_baseline(ctrl_mppi, env)
-
-    ctrl_cem = CEM(env.task,
-                  num_samples=256,
-                  plan_horizon=horizon,
-                  num_elites=10,
-                  sigma_start=0.1,
-                  sigma_min=0.01,
-                  num_knots=knots,
-                  iterations=iters,
-                  spline_type="zero")
-    compute_baseline(ctrl_cem, env)
+    # ctrl_ps = PredictiveSampling(env.task,
+    #               num_samples=256,
+    #               plan_horizon=horizon,
+    #               noise_level=0.1,
+    #               num_knots=knots,
+    #               iterations=iters,
+    #               spline_type="zero")
+    # compute_baseline(filename, ctrl_ps, env, num_compute)
+    #
+    # ctrl_mppi = MPPI(env.task,
+    #               num_samples=256,
+    #               plan_horizon=horizon,
+    #               noise_level=0.1,
+    #               temperature=0.1,
+    #               num_knots=knots,
+    #               iterations=iters,
+    #               spline_type="zero")
+    # compute_baseline(filename, ctrl_mppi, env, num_compute)
+    #
+    # ctrl_cem = CEM(env.task,
+    #               num_samples=256,
+    #               plan_horizon=horizon,
+    #               num_elites=10,
+    #               sigma_start=0.1,
+    #               sigma_min=0.01,
+    #               num_knots=knots,
+    #               iterations=iters,
+    #               spline_type="zero")
+    # compute_baseline(filename, ctrl_cem, env, num_compute)
--- gpc/value_learning/value_baseline.py
+++ gpc/value_learning/value_baseline.py
@@ -1,5 +1,7 @@
 from typing import Tuple, Any
+import time
 import matplotlib.pyplot as plt
+import pickle
 import jax
 import jax.numpy as jnp
 from hydrax.alg_base import SamplingBasedController, Trajectory
@@ -10,33 +12,18 @@ from gpc.envs import TrainingEnv
 
 # Run a very fine traj opt with lots of samples and iterations.
 # The idea is that the trajopt will find the optimal solution so that the resulting cost can be used as the value function.
-
 def compute_value(x: mjx.Data, ctrl: SamplingBasedController, params: Any) -> (
         Tuple)[Any, Trajectory,]:
     """Given a state, compute the value function value and the associated optimal trajectory."""
-    params_1, rollouts_1 = ctrl.optimize(x, params)
+    optimize_jit = jax.jit(ctrl.optimize)
+    params, rollouts = optimize_jit(x, params)
 
-    # # Closed loop rollout
-    # def _scan_fn(carry, _):
-    #     x, params = carry
-    #     params, rollout = ctrl.optimize(x, params)
-    #     min_cost_idx = jnp.argmin(jnp.sum(rollout.costs, axis=1))
-    #     x = x.replace(ctrl=rollout.controls[min_cost_idx, 0])
-    #     x_new = mjx.step(ctrl.model, x)
-    #     return (x_new, params), (rollout.controls[min_cost_idx, 0], rollout.costs[min_cost_idx, 0],
-    #                          rollout.trace_sites[min_cost_idx, 0])
-    #
-    # (xf, params), (U_star, J_star, traces) = jax.lax.scan(_scan_fn, (x, params),
-    #                                                       jnp.arange(ctrl.ctrl_steps))
+    return params, rollouts
 
-    # print(f"J_star shape: {J_star.shape}")
-    # print(f"U_star shape: {U_star.shape}")
-    # print(f"traces shape: {traces.shape}")
-
-    return params, rollouts_1
-
-def compute_baseline(ctrl: SamplingBasedController, env: TrainingEnv) -> jax.Array:
+def compute_baseline(filename: str, ctrl: SamplingBasedController, env: TrainingEnv, num_compute: int) -> jax.Array:
     """Compute the value function baseline for a given controller."""
+
+    start_time = time.perf_counter()
 
     # RNG
     key = jax.random.PRNGKey(0)
@@ -44,39 +31,73 @@ def compute_baseline(ctrl: SamplingBasedController, env: TrainingEnv) -> jax.Arr
     # Initial params
     params = ctrl.init_params()
 
-    # Go through a grid in the statespace
-
-
-    # Get a state
+    # Initial state
     x = env.init_state(key)
-    q = 1.5*jnp.ones(1)
-    x = x.replace(data=x.data.replace(qpos=q))
 
-    # Compute the value at each point
-    params, rollouts = compute_value(x.data, ctrl, params)
+    # Go through a grid in the statespace
+    with open(filename, 'wb') as f:
+        for i in range(num_compute):
+            key = jax.random.PRNGKey(i)
+            # Get a state
+            x = x.replace(data=env.reset(x.data, key))
+            # q = 1.5*jnp.ones(1)
+            # x = x.replace(data=x.data.replace(qpos=q))
 
-    # Single optimization result
-    min_cost_idx = jnp.argmin(jnp.sum(rollouts.costs, axis=1))
-    costs = jnp.sum(rollouts.costs, axis=1)
-    J_star = costs[min_cost_idx]
-    U_star = rollouts.controls[min_cost_idx]
-    print(f"[OL] average rollout cost: {jnp.mean(costs)}, standard dev rollout cost: {jnp.std(costs)}"
-          f" minimum rollout cost: {J_star}")
+            # Compute the value at each point
+            params, rollouts = compute_value(x.data, ctrl, params)
 
-    print(f"trace site shape: {rollouts.trace_sites.shape}")
-    x_traj = rollouts.trace_sites[min_cost_idx, :, 0, :]
+            # Single optimization result
+            min_cost_idx = jnp.argmin(jnp.sum(rollouts.costs, axis=1))
+            costs = jnp.sum(rollouts.costs, axis=1)
+            J_star = costs[min_cost_idx]
+            U_star = rollouts.controls[min_cost_idx]
+            print(f"[OL] average rollout cost: {jnp.mean(costs)}, standard dev rollout cost: {jnp.std(costs)}"
+                  f" minimum rollout cost: {J_star}")
 
-    time = jnp.linspace(params.tk[0], params.tk[-1], U_star.shape[0])
-    time_x = jnp.linspace(params.tk[0], params.tk[-1], x_traj.shape[0])
+            data_chunk = {'J_star': J_star, 'U_star': U_star, 'state': x}
+            pickle.dump(data_chunk, f)
 
-    fig, axes = plt.subplots(4, 1)
-    axes[0].plot(time, U_star)
-    axes[0].set_ylabel("U")
-    for i in range(x_traj.shape[1]):
-        axes[i + 1].plot(time_x, x_traj[:, i])
-        label = ["X", "Y", "Z"]
-        axes[i + 1].set_ylabel(label[i])
-    plt.show()
+    end_time = time.perf_counter()
+    print(f"Time taken: {end_time - start_time}s")
+
+    # x_traj = rollouts.trace_sites[min_cost_idx, :, 0, :]
+
+    # time = jnp.linspace(params.tk[0], params.tk[-1], U_star.shape[0])
+    # time_x = jnp.linspace(params.tk[0], params.tk[-1], x_traj.shape[0])
+
+    # fig, axes = plt.subplots(4, 1)
+    # axes[0].plot(time, U_star)
+    # axes[0].set_ylabel("U")
+    # for i in range(x_traj.shape[1]):
+    #     axes[i + 1].plot(time_x, x_traj[:, i])
+    #     label = ["X", "Y", "Z"]
+    #     axes[i + 1].set_ylabel(label[i])
+    # plt.show()
 
     # Save to a file
     # csv saved as (state, value, trajectory)
+
+def parse_value_data(filename: str) -> None:
+    """Parse the value data from a file."""
+    J_star = jnp.empty(0)
+    U_star = jnp.empty(0)
+    x = jnp.empty(0)
+    y = jnp.empty(0)
+    with open(filename, 'rb') as f:
+        while True:
+            try:
+                data_chunk = pickle.load(f)
+                J_star = jnp.append(J_star, data_chunk['J_star'])
+                U_star = jnp.append(U_star, data_chunk['U_star'])
+                # state = jnp.append(state, data_chunk['state'])
+                state = data_chunk['state']
+                x = jnp.append(x, state.data.qpos[0])
+                y = jnp.append(y, state.data.qvel[0])
+                print("Loading data...")
+            except EOFError:
+                break
+
+    plt.figure()
+    plt.scatter(x, y, c=J_star)
+    plt.colorbar()
+    plt.show()
